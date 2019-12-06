dec662c3316e07b49d4dfc1ef63d51d46adcc7fd
Code4Blessings <robinwarden@code4blessings.com>
2019-12-05 22:19:59 -0500

    Set up dashboard and display files

---
+++ web-application-testing/src/Display.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const Display = () => {
+    return (
+        <div>
+            <h1>Display</h1>
+            <h3></h3>
+        </div>
+    );
+}
+
+export default Display;
